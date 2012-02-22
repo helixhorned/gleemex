9672e05b45c3a662d26c390f1bdc17a9ccea643e
Philipp Kutin <philipp.kutin@gmail.com>
2012-02-22 21:04:48 +0100

    factor out some stuff, 'line-beg/end'

--- simpleplot.m
+++ simpleplot.m
@@ -13,17 +13,11 @@ function simpleplot(data)
     glc = glcall();
     GL = getglconsts();
 
-    %% validation
-    if (~isnumeric(data))
-        error('DATA must be numeric');
-    end
-
-    numdims = size(data, 1);
-    if (numdims ~= 2 && numdims ~= 3)
-        error('DATA must have 2 or 3 ''dimensions'' (i.e. length of 1st dim must be 2 or 3)');
-    end
 
     simpl = struct();
+
+    %% data-data
+    sp_setup_data(data);
 
     %% app data
     simpl.omx = -1;  % old mouse-x position, -1: invalid
@@ -34,24 +28,13 @@ function simpleplot(data)
     simpl.zoom = 1;
     simpl.ang = 0;  % angle around y axis in degrees
 
-
-    %% data-data
-    numsamples = size(data, 2);
-    simpl.data = single(data);
-    if (numdims == 2)
-        simpl.data(3, :) = 0;  % pad 3rd dim with zeros
-    end
-    simpl.mean = mean(simpl.data, 2);
-    simpl.data = simpl.data - repmat(simpl.mean, 1,numsamples);
-    % min and max of mean-subtracted data
-    simpl.min = min(simpl.data, [], 2);
-    simpl.max = max(simpl.data, [], 2);
+    simpl.lineidxs = [1 1];  % beginning and end indices of show-as-line   XXX: blah
 
     % create the window!
     winid = glcall(glc.newwindow, [20 20], simpl.wh, 'Simple plot');
 
 
-    % Initialization requiring a GL state goes here...
+    % Initialization requiring a GL context goes here...
 
     % e.g.
 %    glcall(glc.colormap, uint8(jet(256)' * 255));
@@ -65,6 +48,32 @@ function simpleplot(data)
     glcall(glc.entermainloop);
 end
 
+
+function sp_setup_data(data)
+    global simpl
+
+    %% validation
+    if (~isnumeric(data))
+        error('DATA must be numeric');
+    end
+
+    numdims = size(data, 1);
+    if (numdims ~= 2 && numdims ~= 3)
+        error('DATA must have 2 or 3 ''dimensions'' (i.e. length of 1st dim must be 2 or 3)');
+    end
+
+    simpl.numsamples = size(data, 2);
+    simpl.data = single(data);
+
+    if (numdims == 2)
+        simpl.data(3, :) = 0;  % pad 3rd dim with zeros
+    end
+    simpl.mean = mean(simpl.data, 2);
+    simpl.data = simpl.data - repmat(simpl.mean, 1,simpl.numsamples);
+    % min and max of mean-subtracted data
+    simpl.min = min(simpl.data, [], 2);
+    simpl.max = max(simpl.data, [], 2);
+end
 
 function sp_setup_axes_rect()
     global simpl
@@ -89,9 +98,9 @@ function sp_display()
     glcall(glc.clear, 1-[0 0 0]);
 
     %% draw axes
-    mpone = [-1 1];
     axwh = simpl.axrect([3 4])-simpl.axrect([1 2]);
-    glc_axes_setup(simpl.axrect, [mpone*axwh(1)/axwh(2), -1 1, -1 1]);  % {{{
+    wbyhmpone = [-1 1]*axwh(1)/axwh(2);  % width-by-height minus-plus one
+    glc_axes_setup(simpl.axrect, [wbyhmpone, -1 1, 10*wbyhmpone]);  % {{{
 
     maxmaxabs = max(max(abs(simpl.min), abs(simpl.max)));
     scalemat = simpl.zoom.*eye(4)./double(maxmaxabs);
@@ -100,15 +109,31 @@ function sp_display()
 
     glcall(glc.mulmatrix, GL.MODELVIEW, [simpl.ang, 0 1 0]);
 
+    if (simpl.lineidxs(2) > simpl.lineidxs(1))
+        glcall(glc.draw, GL.LINE_STRIP, simpl.data(:, simpl.lineidxs(1):simpl.lineidxs(2)), ...
+               struct('colors',[.2 .2 .2]));
+    end
     glcall(glc.draw, GL.POINTS, simpl.data);
 
     glc_axes_finish([0 0 0]); % }}}
 end
 
+function cval = sp_clamp(val, themin, themax)
+    cval = min(max(themin, val), themax);
+end
+
 function sp_keyboard(key, x, y, mods)
     global simpl GL glc
 
-    % ...
+    mul = (1+9*(~~bitand(mods,GL.MOD_CTRL) + 6*(~~bitand(mods,GL.MOD_SHIFT))));
+    switch key
+      case { GL.KEY_RIGHT, GL.KEY_LEFT }
+        simpl.lineidxs(2) = simpl.lineidxs(2) + mul*(1-2*(key==GL.KEY_LEFT));
+        simpl.lineidxs(2) = sp_clamp(simpl.lineidxs(2), simpl.lineidxs(1),simpl.numsamples);
+      case { GL.KEY_UP, GL.KEY_DOWN }
+        simpl.lineidxs(1) = simpl.lineidxs(1) + mul*(1-2*(key==GL.KEY_DOWN));
+        simpl.lineidxs(1) = sp_clamp(simpl.lineidxs(1), 1,simpl.lineidxs(2));
+    end
 
     glcall(glc.postredisplay);
 end
@@ -143,9 +168,10 @@ function sp_mouse(button, downp, x, y, mods)
     if (downp)
         switch button
           case GL.MWHEEL_UP,
-            simpl.zoom = simpl.zoom * 1.1;
+            % XXX: ctrl doesn't work here (linux)
+            simpl.zoom = simpl.zoom * (1.2 + 0.3*(mods==GL.MOD_CTRL));
           case GL.MWHEEL_DOWN,
-            simpl.zoom = simpl.zoom / 1.1;
+            simpl.zoom = simpl.zoom / (1.2 + 0.3*(mods==GL.MOD_CTRL));
         end
     else
         simpl.omx = -1;
