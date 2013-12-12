8abc2b3160a040d20bab1b2bb70e32886b01312f
Philipp Kutin <philipp.kutin@gmail.com>
2013-12-12 15:00:59 +0100

    Add glcshell.m and classdef class GLCEditableLine.

---
+++ GLCEditableLine.m
@@ -0,0 +1,147 @@
+classdef GLCEditableLine < handle
+    properties
+        dohist  % enable history?
+        linehist  % the history of formerly executed lines
+
+        curi  % the index of the '|' cursor in .curline
+        curline  % the currently edited line
+    end
+
+    methods
+        % SELF = GLCEditableLine(DOHIST)
+        %
+        % Constructor.
+        %  DOHIST: scalar logical, enable history?
+        function self = GLCEditableLine(dohist)
+            if (~(isscalar(dohist) && islogical(dohist)))
+                error('DOHIST must be a scalar logical')
+            end
+
+            self.dohist = dohist;
+            self.linehist = {};
+
+            self.curi = 1;
+            self.curline = '|';
+        end
+
+        %% Methods to be run from Gleemex callbacks
+
+        % .display(XRANGE, YORIGIN [, OPTS])
+        function display(self, xrange, yorigin, opts)
+            global GL glc
+
+            if (nargin < 4)
+                opts = struct();
+            end
+
+            glc_textlines({self.curline}, 1, xrange, yorigin, opts);
+        end
+
+        function doquit = handleKey(self, key, mods)
+            global GL glc
+
+            doquit = false;
+
+            if (key >= 32 && key < 127)
+                % printable char
+                self.curins(key);
+                return
+            elseif (key == GL.KEY_ENTER)
+                doquit = self.issueCommand();
+                return
+            end
+
+            switch (key)
+              case GL.KEY_BACKSPACE,
+                self.curdelbefore();
+              case GL.KEY_DELETE,
+                self.curdelafter();
+              case GL.KEY_LEFT,
+                self.curleft();
+              case GL.KEY_RIGHT,
+                self.curright();
+              case { GL.KEY_HOME, 1 },  % HOME, Ctrl-A
+                self.curbeg();
+              case { GL.KEY_END, 5 },  % END, Ctrl-E
+                self.curend();
+            end
+
+%            fprintf('%d\n', key);
+        end
+    end
+
+    methods (Access=protected)
+        function doquit = issueCommand(self)
+            doquit = false;
+
+            cmd = strtrim(self.curline([1:self.curi-1 self.curi+1:end]));
+            if (isempty(cmd))
+                return
+            end
+
+            if (any(strcmp(cmd, {'quit', 'exit'})))
+                doquit = true;
+                return
+            end
+
+            try
+                evalin('base', cmd);
+            catch
+                fprintf('%s\n', lasterr());
+            end
+
+            % clear command line
+            self.curi = 1;
+            self.curline = '|';
+        end
+
+        %% Cursor movement etc.
+
+        % delete char before cursor
+        function curdelbefore(self)
+            if (self.curi > 1)
+                self.curline(self.curi-1) = [];
+                self.curi = self.curi-1;
+            end
+        end
+
+        % delete char after cursor
+        function curdelafter(self)
+            if (self.curi < numel(self.curline))
+                self.curline(self.curi+1) = [];
+            end
+        end
+
+        function curins(self, asc)
+            curline = self.curline;
+            self.curline = [curline(1:self.curi-1) asc '|' curline(self.curi+1:end)];
+            self.curi = self.curi+1;
+        end
+
+        function curleft(self)
+            if (self.curi > 1)
+                self.curline([self.curi-1 self.curi]) = self.curline([self.curi self.curi-1]);
+                self.curi = self.curi-1;
+            end
+        end
+
+        function curright(self)
+            if (self.curi < numel(self.curline))
+                self.curline([self.curi+1 self.curi]) = self.curline([self.curi self.curi+1]);
+                self.curi = self.curi+1;
+            end
+        end
+
+        function curbeg(self)
+            while (self.curi > 1)
+                self.curleft();
+            end
+        end
+
+        function curend(self)
+            while (self.curi < numel(self.curline))
+                self.curright();
+            end
+        end
+    end
+end
---
+++ glcshell.m
@@ -0,0 +1,73 @@
+% Gleemex shell.
+%
+function glcshell(data)
+    global glcsh GL glc
+
+    glc = glcall();
+    GL = glconstants();
+
+    glcsh = struct();
+
+    glcsh.mxy = [0 0];
+    glcsh.wh = [400 600];
+
+    % App data
+    glcsh.el = GLCEditableLine(true);
+
+    % create the window!
+    winid = glcall(glc.newwindow, [20 20], glcsh.wh, 'Gleemex shell');
+
+    glcall(glc.setcallback, glc.cb_reshape, 'sh_reshape');
+    glcall(glc.setcallback, glc.cb_display, 'sh_display');
+    glcall(glc.setcallback, glc.cb_keyboard, 'sh_keyboard');
+    glcall(glc.setcallback, glc.cb_motion, 'sh_motion');
+    glcall(glc.setcallback, glc.cb_mouse, 'sh_mouse');
+    glcall(glc.entermainloop);
+end
+
+
+function sh_reshape(w, h)
+    global glcsh GL glc
+
+    glcsh.wh = [w h];
+
+    glc_setup2d(w, h);
+end
+
+function sh_display()
+    global glcsh GL glc
+
+    glcall(glc.clear, 1-[0 0 0]);
+
+    w = glcsh.wh(1);
+    glcsh.el.display([20 w-20], 20+16, struct('lineheight',16, 'colors',[.9 .9 .9]));
+end
+
+function sh_keyboard(key, x, y, mods)
+    global glcsh GL glc
+
+    doquit = glcsh.el.handleKey(key, mods);
+    if (doquit)
+        glcall(glc.closewindow);
+        return
+    end
+
+    glcall(glc.redisplay);
+end
+
+function sh_motion(buttonsdown, x, y)
+    global glcsh GL glc
+
+    % invert y
+    glcsh.mxy = [x glcsh.wh(2)-y];
+
+    % ...
+
+    glcall(glc.redisplay);
+end
+
+function sh_mouse(button, downp, x, y, mods)
+    global glcsh GL glc
+
+    % ...
+end
