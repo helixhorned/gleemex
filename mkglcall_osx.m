92c300450f23555b3fce756c04ede4952c3ae6b8
Philipp Kutin <helixhorned@gmail.com>
2014-05-14 16:15:06 +0300

    mkglcall_osx.m: update to assume GLEW and FreeGLUT from Homebrew.

--- mkglcall_osx.m
+++ mkglcall_osx.m
@@ -1,5 +1,2 @@
-#mkoctfile -v --mex -I/usr/X11/include -L/usr/X11/lib -Wall -Wextra -framework OpenGL -lXext -lX11 -lXrandr -lXxf86vm -framework GLUT -lGL -lGLEW glcall.c /usr/local/lib/libglut.a
-#mkoctfile -v --mex -I/usr/X11/include -L/usr/X11/lib -Wall -Wextra -lXext -lX11 -lXrandr -lXxf86vm -lGL -L/opt/local/lib -lGLEW -lglut -lXi -lGLU glcall.c /opt/local/lib/libglut.a
-#mkoctfile -v --mex -Wall -Wextra -lXext -lX11 -lXrandr -lXxf86vm -lGL -L/opt/local/lib -lGLEW -lglut -lXi -lGLU glcall.c /opt/local/lib/libglut.a
-# FreeGLUT, GLEW from MacPorts
-mkoctfile -v --mex -Wall -Wextra -lX11 -lXrandr -lXxf86vm -lGL -L/opt/local/lib -lGLEW -lglut -lGLU glcall.c
+## FreeGLUT and GLEW from Homebrew
+mkoctfile -v --mex -Wall -Wextra -lX11 -lXrandr -lXxf86vm -lGL -I/usr/local/include -L/usr/local/lib -lGLEW -lglut -lGLU glcall.c
