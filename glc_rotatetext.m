5738954dc7141e1fc8a18cf49e129c3c3c410dc2
Philipp Kutin <philipp.kutin@gmail.com>
2014-05-17 15:53:33 +0200

    glc_rotatetext: add two input arg checks.

--- glc_rotatetext.m
+++ glc_rotatetext.m
@@ -4,8 +4,11 @@
 function glc_rotatetext(xy_origin, ang, ofs, height, label, varargin)
     global glc GL
 
+    assert(isnumeric(xy_origin) && numel(xy_origin)==2, 'XY_ORIGIN must be a numeric pair')
+    assert(isnumeric(ang) && numel(ang)==1, 'ANG must be a numeric scalar')
+
     glcall(glc.push, GL.MODELVIEW);
-    glcall(glc.mulmatrix, GL.MODELVIEW, [xy_origin 0]);  % translate text origin
+    glcall(glc.mulmatrix, GL.MODELVIEW, [xy_origin(:).' 0]);  % translate text origin
     glcall(glc.mulmatrix, GL.MODELVIEW, [ang, 0 0 1]);  % rotate wrt origin
     glcall(glc.text, ofs, height, label, varargin{:});
     glcall(glc.pop, GL.MODELVIEW);
