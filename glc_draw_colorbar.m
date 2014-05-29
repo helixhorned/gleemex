67d7d19c9cf48ce9dac174a2ce048938d550eed1
Philipp Kutin <philipp.kutin@gmail.com>
2014-05-29 12:49:40 +0200

    glc_draw_colorbar: add the option of using glc_genticks (negative NUMTICKS).

--- glc_draw_colorbar.m
+++ glc_draw_colorbar.m
@@ -1,13 +1,15 @@
 % GLC_DRAW_COLORBAR(RECT, BOUNDS, LABEL, CMAPTEXNAME [, FORMAT [, NUMTICKS [, TEXTHEIGHT]]])
 %
-%  RECT - [x1 y1 x2 y2] bounding box
-%  BOUNDS - [min max]
-%  LABEL - the string to print near the color bar
-%  CMAPTEXNAME - the color map 2D texture name (ID)
-%  FORMAT - defaults to '%.02f'
-%  NUMTICKS - the number of tick marks, divided between min and max
-%             bound. Default: 7
-%  TEXTHEIGHT - the text height passed to glc.text.  Default: 10
+% RECT: [x1 y1 x2 y2] bounding box
+% BOUNDS: [min max]
+% LABEL: the string to print near the color bar
+% CMAPTEXNAME: the color map 2D texture name (ID)
+% FORMAT: defaults to '%.02f'
+% NUMTICKS: the number of tick marks, divided between min and max
+%  bound. If negative, glc_genticks() is used with -NUMTICKS ticks.
+%  Default: 7
+% TEXTHEIGHT: the text height passed to glcall(glc.text, ...).
+%  Default: 10
 function glc_draw_colorbar(rect, bounds, label, cmaptexname, format, numticks, textheight)
     global glc GL
 
@@ -32,11 +34,16 @@ function glc_draw_colorbar(rect, bounds, label, cmaptexname, format, numticks, t
         glc_rotatetext(xy_origin, 90,  [0 16], 14, label, [0 -1]);
     end
 
-    for i=0:numticks-1
-        frac = (i/(numticks-1));
-        y = rect(2) + frac*(rect(4)-rect(2)); % - textheight/2;
+    if (numticks < 0)
+        ticks = glc_genticks(bounds, -numticks);
+        glc_drawticks(rect([3 2 3 4]), bounds, ticks, -3, -textheight);
+    else
+        for i=0:numticks-1
+            frac = (i/(numticks-1));
+            y = rect(2) + frac*(rect(4)-rect(2));
 
-        glcall(glc.text, [rect(3)+8, y], textheight, ...
-               sprintf(format, bounds(1)+frac*(bounds(2)-bounds(1))), [-1 0]);
+            glcall(glc.text, [rect(3)+8, y], textheight, ...
+                   sprintf(format, bounds(1)+frac*(bounds(2)-bounds(1))), [-1 0]);
+        end
     end
 end
